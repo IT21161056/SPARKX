6556a6fc7f40ff2bfeada15738e0b89afe4ef3d6
Pasindu Perakum <it21160066my.sliit.lk>
2023-10-23 13:20:49 +0530

    changes

--- frontend/screens/ElectricianReg.jsx
+++ frontend/screens/ElectricianReg.jsx
@@ -1,144 +1,170 @@
 import React, { useState } from 'react';
-import { useNavigation } from "@react-navigation/native";
-import { 
-    SafeAreaView, 
-    ScrollView, 
-    View,
-    StyleSheet,
-    Image,
-    Text,
-    TouchableOpacity,
-    TextInput, 
-} from 'react-native';
 import { SIZES, COLORS } from "../constants/theme";
-import Modal from 'react-native-modal';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-const ElectricianReg = () => {
-    
-  const navigation = useNavigation();
-  const [isModalVisible, setModalVisible] = useState(false);
+export default function ElectricianRegistrationScreen() {
+  const [fullName, setFullName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [age, setAge] = useState('');
+  const [qualifications, setQualifications] = useState('');
+  const [image, setImage] = useState(null);
 
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
+  const selectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== 'granted') {
+      console.error('Permission to access image library was denied.');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync();
+
+    if (!result.canceled) {
+      setImage(result.uri);
+    }
   };
 
-  const handleRegistration = () => {
-    // Show the success modal
-    toggleModal();
 
-    // Automatically close the modal after 3 seconds
-    setTimeout(() => {
-      toggleModal();
-      // Navigate to the next screen
-      navigation.navigate('Electricians', { electricianData });
-    }, 4000);  // 4000 milliseconds (3 seconds) delay
+  const handleRegistration = () => {
+    console.log('Registration Data:', {
+      fullName,
+      contactNumber,
+      address,
+      age,
+      qualifications,
+      image,
+    });
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
-        <ScrollView>
-           <View style={styles.mainView}>
-                <View style={styles.container0}>
+    <ScrollView style={styles.container}>
+      <View>
 
-                </View>
-                <View>
-                    <TextInput
-                        placeholder='Enter Full Name *'
-                        style={styles.input}
-                    />
-                    <TextInput
-                        placeholder='Enter Mobile Number *'
-                        style={styles.input}
-                    />
-                    <TextInput
-                        placeholder='Enter Address *'
-                        style={styles.input}
-                    />
-                    <TextInput
-                        placeholder='Enter Age *'
-                        style={styles.input}
-                    />
-                    <TextInput
-                        placeholder='Enter qualifications and experience *'
-                        style={styles.inputLarger}
-                    />
-                    <TouchableOpacity 
-                        style={styles.submitButton}
-                        onPress={handleRegistration}
-                    >
-                        <Text style={styles.buttonText}>
-                            Register
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-           </View> 
-        </ScrollView>
-    </SafeAreaView>
-  )
+         <View style={styles.behindImage}>
+          <View style={styles.imageContainer}>
+            <TouchableOpacity style={styles.imageUploadButton} onPress={selectImage}>
+              {image ? (
+                <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
+              ) : (
+                <Text>Add Photo</Text>
+              )}
+            </TouchableOpacity>
+          </View>
+         </View>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Full Name *"
+          value={fullName}
+          onChangeText={(text) => setFullName(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Contact Number *"
+          value={contactNumber}
+          onChangeText={(text) => setContactNumber(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Address *"
+          value={address}
+          onChangeText={(text) => setAddress(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Age *"
+          value={age}
+          onChangeText={(text) => setAge(text)}
+        />
+
+        <TextInput
+          style={styles.inputLarger}
+          placeholder="Enter qualifications and experience *"
+          value={qualifications}
+          onChangeText={(text) => setQualifications(text)}
+        />
+
+        <TouchableOpacity style={styles.submitButton} onPress={handleRegistration}>
+          <Text  style={styles.buttonText}>Submit Details</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 }
 
-export default ElectricianReg;
-
 const styles = StyleSheet.create({
-    mainView:{
-        flex: 1,
-        padding: SIZES.large,
-        width: "100%"
-    },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  behindImage:{
 
-    submitButton:{
-        alignItems: "center",
-        justifyContent: "center",
-        width: '100%',
-        height: 40, 
-        backgroundColor: "#096FCC",
-        borderRadius: SIZES.small,
-        marginTop: 20
-    },
-
-    buttonText:{
-        color: "white",
-        fontSize: SIZES.medium,
-        textAlign: "center",
-    },
-
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  imageUploadButton: {
+    width: 150, 
+    height: 150, 
+    backgroundColor: '#e0e0e0', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  imagePreview: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  imageUploadText: {
+    fontSize: 16,
+    color: 'blue', 
+  },
   input: {
+    height: 50,
+    marginBottom: 10,
+    paddingLeft: 10,
     fontSize: SIZES.medium,
     borderWidth: 0.4,
     borderColor: '#096FCC',
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginTop: 10,
     borderRadius: 15,
     backgroundColor: "#fff",
-    elevation: 10,
+    elevation: 9,
   },
-  inputLarger: {
-    fontSize: 16,
-    borderWidth: 0.4,
-    borderColor: '#096FCC',
-    borderRadius: 15,
-    paddingVertical: 25, // Adjust the height as needed
-    paddingHorizontal: 10,
-    marginTop: 10,
-    backgroundColor: "#fff",
-    elevation: 10
+    inputLarger: {
+      fontSize: 16,
+      borderWidth: 0.4,
+      borderColor: '#096FCC',
+      borderRadius: 15,
+      paddingVertical: 25,
+      paddingHorizontal: 10,
+      marginTop: 10,
+      backgroundColor: "#fff",
+      elevation: 10
   },
-  container0: {
-    flex: 1,
-    width: "100%",
-    height: 150,
-    flexDirection: "column",
-    borderRadius: SIZES.medium,
-    shadowColor: "#000",
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    backgroundColor: "#122B5Cff",
-    //marginLeft: 10,
-    elevation: 3,
-    borderWidth: 0.4,
-    borderColor: "#096FCC",
+  submitButton:{
+      alignItems: "center",
+      justifyContent: "center",
+      width: '100%',
+      height: 40, 
+      backgroundColor: "#096FCC",
+      borderRadius: SIZES.small,
+      marginTop: 50
   },
-})
+  buttonText:{
+      color: "white",
+      fontSize: SIZES.medium,
+      textAlign: "center",
+  },
+});
